33ba9dd07f546045813868340277a5c9e920701a
a8hok <ashokkumar.explore@gmail.com>
2022-01-17 14:37:49 +0530

    removed unwanted files

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import Card from "../components/Card";
+
+const Home = () => {
+    return (
+        <div className="home">
+            
+        </div>
+    )
+}
+
+export default Home;
---
+++ client/src/routes/Login.jsx
@@ -0,0 +1,24 @@
+import Github from "../img/github.png";
+
+const Login = () => {
+
+  const github = () => {
+    window.open("http://localhost:5000/auth/github", "_self");
+  };
+
+
+  return (
+    <div className="login">
+      <div className="wrapper">
+        <div className="left">
+          <div className="loginButton github" onClick={github}>
+            <img src={Github} alt="" className="icon" />
+            Github
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
