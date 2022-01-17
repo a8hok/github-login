ab05840908edbdd3a26a6f05ca5172dbb693c275
a8hok <ashokkumar.explore@gmail.com>
2022-01-17 13:52:30 +0530

    auth server for github added

---
+++ server/passport.js
@@ -0,0 +1,27 @@
+const GithubStrategy = require("passport-github2").Strategy;
+const passport = require("passport");
+
+GITHUB_CLIENT_ID = "ID";
+GITHUB_CLIENT_SECRET = "secret";
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+        console.log(profile);
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
